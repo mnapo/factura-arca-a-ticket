2b9221fc0f26841d6334826357c7f8eea8656dd9
napo <napolimatiase@gmail.com>
2025-04-10 04:03:23 -0300

    Definir endpoints

--- server.js
+++ server.js
@@ -3,6 +3,27 @@ const fastify = require("fastify")({logger: false,});
 
 fastify.register(require("@fastify/static"), {root: path.join(__dirname, "public"), prefix: "/",});
 
+fastify.get("/", function (request, reply) {return reply.view("/src/index.html");});
+fastify.get("/ticket", function (request, reply) {return reply.view("src/ticket.html");});
+
+async function parseFile(reply) {}
+
+fastify.post("/fileupload", async function (request, reply) {
+  let form = new formidable.IncomingForm();
+  let params = {};
+  const parts = request.parts();
+  try{
+    for await (const part of parts) {
+      if (part.file){
+        newFilename = part.filename;
+        await pump(part.file, fs.createWriteStream("public/factura.pdf"))
+        params = await parseFile(reply).then(()=>{
+        });
+      }
+    }
+  } finally{}
+});
+
 fastify.listen(
   {port: process.env.PORT, host: "0.0.0.0"},
   function (err, address) {
